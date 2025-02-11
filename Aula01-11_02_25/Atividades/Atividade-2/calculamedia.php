1f8e779cdef4b886e656963da79795e5d583033d
Murilosgama <murilosgama@gmail.com>
2025-02-11 11:10:40 -0300

    salvo

--- Aula01-11_02_25/Atividades/Atividade-2/calculamedia.php
+++ Aula01-11_02_25/Atividades/Atividade-2/calculamedia.php
@@ -1,4 +1,13 @@
 <?php
-    
-
+    if (isset($_GET['nota1']) && isset($_GET['nota2']) && isset($_GET['nota3'])) {
+        $nota1 = floatval($_GET['nota1']);
+        $nota2 = floatval($_GET['nota2']);
+        $nota3 = floatval($_GET['nota3']);
+        
+        $media = ($nota1 + $nota2 + $nota3) / 3;
+        
+        echo "A sua média é: " . number_format($media, 2);
+    } else {
+        echo "Por favor, forneça três notas na URL, por exemplo: calculamedia.php?nota1=7&nota2=8.5&nota3=6";
+    }
 ?>
