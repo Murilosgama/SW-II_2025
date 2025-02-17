993cd015f940d61d07939ddce1f3f1ad5ca58881
Murilosgama <murilosgama@gmail.com>
2025-02-17 11:08:59 -0300

    Salvo

---
+++ Aula02-17_02_25/Atividades/Atividade02.php
@@ -0,0 +1,11 @@
+<?php
+$numero = 0;
+
+if ($numero > 0) {
+    echo "Seu número é POSITIVO.";
+} else if ($numero < 0) {
+    echo "Seu número é NEGATIVO.";
+} else {
+    echo "Seu número é ZERO.";
+}
+?>
