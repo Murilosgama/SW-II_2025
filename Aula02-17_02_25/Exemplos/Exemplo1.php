f56598b3b9ff5803b680aa36d2234c6a2d20a5ce
Murilosgama <murilosgama@gmail.com>
2025-02-17 09:38:12 -0300

    salvo

--- Aula02-17_02_25/Exemplos/Exemplo1.php
+++ Aula02-17_02_25/Exemplos/Exemplo1.php
@@ -2,5 +2,6 @@
 $a = 10;
 $b = 5;
 $soma = $a + $b;
-echo "A soma é: $soma"; // Saída: A soma é: 15
+echo "A soma é: $soma <br>"; // Saída: A soma é: 15
+echo "A soma é: ". $soma; // Saída: A soma é: 15, mas embaixo do primeiro por causa do "<br>"
 ?>
---
+++ Aula02-17_02_25/Exemplos/Exemplo2.php
@@ -0,0 +1,9 @@
+<?php
+$idade = 18;
+if ($ideda >= 18) {
+    echo "Maior de idade";
+} 
+else {
+    echo "Menor de idade";
+}
+?>
